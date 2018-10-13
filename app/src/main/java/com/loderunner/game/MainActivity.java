26a0933bdeca5d70fad4547bd2951f9cde75f65b
Pinkieqt <pinkfluffy12@seznam.cz>
2018-10-13 18:10:47 +0200

    initial commint

---
+++ app/src/main/java/com/loderunner/game/MainActivity.java
@@ -0,0 +1,14 @@
+package com.loderunner.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hmmm this is my test, does Github even work?
+    }
+}
